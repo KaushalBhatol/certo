ed8810c2aa32b47eced6feca5fe4e16984b0a244
KaushalBhatol <kaushal@dev.bhatol.com>
2026-03-27 23:28:08 +0000

    fixed for prod

--- app.py
+++ app.py
@@ -1264,4 +1264,4 @@ def page_not_found(e):
     return render_template("404.html"), 404
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=8080, ssl_context=(CERT_PATH, KEY_PATH))
+    app.run(debug=False, host="0.0.0.0", port=8080, ssl_context=(CERT_PATH, KEY_PATH))
